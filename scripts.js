0ab3945664d0dad9351c0b8adfe50371e1f59f7d
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-07-09 10:58:03 -0500

    validate word and check for win

--- scripts.js
+++ scripts.js
@@ -6,21 +6,67 @@ async function getWord() {
       throw new Error(`Response status: ${response.status}`);
     }
     const json = await response.json();
-    console.log(json.word);
+    // console.log(json.word);
     return json.word;
   } catch (error) {
     console.error(error.message);
   }
 }
 
-const answer = getWord();
+async function validateWord(guess) {
+  const url = "https://words.dev-apis.com/validate-word";
+  try {
+    const response = await fetch(url,{
+      method: 'POST', 
+      body: `{"word": "${guess}"}`, 
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    // console.log(json.validWord);
+    return json.validWord;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
+async function handleSubmit() {
+  const answer = await getWord();
+  // console.log(answer);
+  const isValid = await validateWord(guess);
+  // console.log(isValid);
+  // validate
+  if (guess.length < 5) {
+    console.log('too short');
+  }
+  if (!isValid) {
+    console.log('not valid word');
+  }
+  
+  // check for a win
+  if (guess === answer) {
+    console.log('you got it!')
+  }
+}
+
+let guess;
 const forms = document.querySelectorAll('form');
-
 forms.forEach(form => {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log('Form submitted');
+    
+    const inputs = form.querySelectorAll('input');
+    const values = [];
+    inputs.forEach(input => values.push(input.value));
+    guess = values.join('');
+
+    handleSubmit();
   });
 });
+
+
+
+
+
 
