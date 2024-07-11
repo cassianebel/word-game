bcf4f4ac33009475fdcf617d79e236285e134ac9
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-07-10 21:42:48 -0500

    refactor to use a workingGuess string

--- script.js
+++ script.js
@@ -35,82 +35,83 @@ function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
 
+let workingGuess = '';
+
 document.addEventListener('keydown', async (event) => {
   const spaces = document.querySelectorAll('.letter:not(.disabled)');
-  const spacesArray = Array.from(spaces);
-  // if a letter is typed - place it in the first available spot
-  if (isLetter(event.key)) {
-    let index = null;
-    for (let i = 0; i < spaces.length; i++) {
-      if (spaces[i].innerText === '') {
-        index = i;
-        break; // Exit the for loop
-      }
-    }
-    if (index !== null) {
-      spaces[index].innerText = event.key;
-    }
-  // if a backspace is typed - delete the last letter
-  } else if (event.key === "Backspace"){
-    let index = null;
-    for (let i = 0; i < spaces.length; i++) {
-      if (spaces[i].innerText === '') {
-        index = i - 1;
-        break; // Exit the for loop
-      }
-    }
-    if (index !== null && index !== -1) {
-      spaces[index].innerText = '';
-    }
-  // if enter is typed, check all the things
-  } else if (event.key === "Enter" && spaces.length > 0) {
-    const guessArray = [];
-    let isFive = false;
-    // check for 5 letters and create the guess array
-    for (let i = 0; i < 5; i++) {
-      if (!spaces[i].innerText) {
-        console.log('must be a 5 letter word')
-        break;
+
+  // if they haven't used all their guesses
+  if (spaces.length > 0) {
+    // if a letter is typed - place it in the first available spot
+    if (isLetter(event.key)) {
+      if (workingGuess.length < 5) {
+        // add last letter to the end of guess
+        workingGuess += event.key;
       } else {
-        guessArray.push(spaces[i].innerText)
+        // replace the last letter once they hit 5 letters
+        workingGuess = workingGuess.substring(0, workingGuess.length - 1) + event.key;
       }
-      isFive = true;
-    }
-    // check if the guess is a valid word
-    const guess = guessArray.join('');
-    const isValid = await validateWord(guess);
-    if (!isValid) {
-      console.log('must be a valid word');
-    }
-    // if the guess is valid, check for matching letters
-    const answer = await getWord(); 
-    let answerArray = answer.split('');
-    if (isFive && isValid) {
-      // check for exact matches
-      for (let i = 0; i < answerArray.length; i++) {
-        if (spaces[i].innerText === answerArray[i]) {
-          spaces[i].classList.add('match');
-          answerArray.splice(i, 1, 'matched');
+      spaces[workingGuess.length - 1].innerText = event.key;
+
+    // if a backspace is typed - delete the last letter
+    } else if (event.key === "Backspace"){
+      spaces[workingGuess.length - 1].innerText = '';
+      workingGuess = workingGuess.substring(0, workingGuess.length - 1);
+
+    // if enter is typed and they haven't used all their guesses - check all the things
+    } else if (event.key === "Enter") {
+
+      // check for 5 letters and create the guess array
+      if (workingGuess.length < 5) {
+        console.log('must be 5 letter word');
+      }
+
+      // check if the guess is a valid word
+      const isValid = await validateWord(workingGuess);
+      if (!isValid) {
+        console.log('must be a valid word');
+      }
+
+      const answer = await getWord(); 
+      // create answer array to track the letter matches
+      let answerArray = answer.split('');
+
+      // if the guess is valid, check for matching letters
+      if (workingGuess.length === 5 && isValid) {
+
+        // check for exact matches
+        for (let i = 0; i < answerArray.length; i++) {
+          if (workingGuess[i] === answerArray[i]) {
+            spaces[i].classList.add('match');
+            answerArray.splice(i, 1, 'matched');
+          }
         }
-      }
-      // check for partial matches
-      for (let i = 0; i < answerArray.length; i++) {
-        if (answerArray.includes(spaces[i].innerText)) {
-          spaces[i].classList.add('partial');
-          answerArray.splice(answerArray.indexOf(spaces[i].innerText), 1, 'matched');
+
+        // check for partial matches
+        for (let i = 0; i < answerArray.length; i++) {
+          if (answerArray.includes(spaces[i].innerText)) {
+            spaces[i].classList.add('partial');
+            answerArray.splice(answerArray.indexOf(spaces[i].innerText), 1, 'partial');
+          }
         }
+
+        // check for a win
+        if (workingGuess === answer) {
+          console.log("You got it!");
+        }
+
+        // disable the 5 letters for this guess (removes them from spaces list)
+        for (let i = 0; i < 5; i++) {
+          spaces[i].classList.add('disabled');
+        }
+
+        // reset the guess
+        workingGuess = '';
       }
-      // check for a win
-      if (guess === answer) {
-        console.log("You got it!");
-      }
-      // disable the 5 letters for this guess (removes them from spaces list)
-      for (let i = 0; i < 5; i++) {
-        spaces[i].classList.add('disabled');
-      }
+
+    // if anything else is typed ignore it
+    } else if (!isLetter(event.key)) {
+      event.preventDefault();
     }
-  // if anything else is typed ignore it
-  } else if (!isLetter(event.key)) {
-    event.preventDefault();
   }
 })
