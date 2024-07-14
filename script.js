4d593d18b34a2bc1879bfc64467c5b9f3d74e14c
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-07-13 21:44:00 -0500

    add onscreen keyboard and touch functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const message = document.querySelector('.message');
+const keys = document.querySelectorAll('.key');
 let workingGuess = '';
 let answer;
 
@@ -46,31 +47,31 @@ async function init() {
 
 init();
 
-document.addEventListener('keydown', async (event) => {
+async function handleKeyPress(key) {
   const spaces = document.querySelectorAll('.letter:not(.disabled)');
 
   // if they haven't used all their guesses
   if (spaces.length > 0) {
     // if a letter is typed - place it in the first available spot
-    if (isLetter(event.key)) {
+    if (isLetter(key)) {
       if (workingGuess.length < 5) {
         // add last letter to the end of guess
-        workingGuess += event.key;
+        workingGuess += key;
       } else {
         // if another letter is typed after 5 replace the last letter
-        workingGuess = workingGuess.substring(0, workingGuess.length - 1) + event.key;
+        workingGuess = workingGuess.substring(0, workingGuess.length - 1) + key;
       }
-      spaces[workingGuess.length - 1].innerText = event.key;
+      spaces[workingGuess.length - 1].innerText = key;
 
     // if a backspace is typed - delete the last letter
-    } else if (event.key === "Backspace"){
+    } else if (key === "Backspace"){
       // remove it from the DOM
       spaces[workingGuess.length - 1].innerText = '';
       // remove it from the workingGuess string
       workingGuess = workingGuess.substring(0, workingGuess.length - 1);
 
     // if enter is typed (and they haven't used all their guesses) - check all the things
-    } else if (event.key === "Enter") {
+    } else if (key === "Enter") {
 
       // check for 5 letters 
       if (workingGuess.length < 5) {
@@ -88,32 +89,35 @@ document.addEventListener('keydown', async (event) => {
       }
 
       // create answer array to track the letter matches
-      let answerArray = answer.split('');
+      let answerArray = answer.toUpperCase().split('');
 
       // if the guess is valid, check for matching letters
       if (workingGuess.length === 5 && isValid) {
         // hide the message alerts
         message.classList.add('hide');
+
         // check for exact matches
         for (let i = 0; i < answerArray.length; i++) {
-          if (workingGuess[i] === answerArray[i]) {
+          if (workingGuess[i].toUpperCase() === answerArray[i].toUpperCase()) {
             spaces[i].classList.add('match');
             answerArray.splice(i, 1, 'matched');
+            document.getElementById(workingGuess[i].toUpperCase()).classList.add('matched');
           }
         }
 
         // check for partial matches
         for (let i = 0; i < answerArray.length; i++) {
-          if (answerArray.includes(spaces[i].innerText)) {
+          if (answerArray.includes(spaces[i].innerText.toUpperCase())) {
             spaces[i].classList.add('partial');
             answerArray.splice(answerArray.indexOf(spaces[i].innerText), 1, 'partial');
+            document.getElementById(workingGuess[i].toUpperCase()).classList.add('partial');
           } else {
-            // spaces[i].classList.add('nope');
+            document.getElementById(workingGuess[i].toUpperCase()).classList.add('used');
           }
         }
 
         // check for a win
-        if (workingGuess === answer) {
+        if (workingGuess.toUpperCase() === answer.toUpperCase()) {
           message.classList.remove('hide', 'alert');
           message.classList.add('win');
           message.innerText = "Impressive!"
@@ -129,8 +133,21 @@ document.addEventListener('keydown', async (event) => {
       }
 
     // if anything else is typed ignore it
-    } else if (!isLetter(event.key)) {
-      event.preventDefault();
+    } else if (!isLetter(key)) {
+      // ignore any other key presses
     }
   }
+}
+
+document.addEventListener('keydown', (event) => {
+  let key = event.key;
+  handleKeyPress(key);
 })
+
+keys.forEach(key => {
+  key.addEventListener('click', (event) => {
+    let key = event.target.innerText;
+    handleKeyPress(key);
+  })
+});
+
